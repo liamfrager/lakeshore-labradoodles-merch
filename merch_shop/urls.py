fc8083e10c972e8da75aa1844a101bb5a9b14fe5
Liam Frager <liam.frager@gmail.com>
2024-08-14 16:03:23 -0400

    remove test_email route

--- merch_shop/urls.py
+++ merch_shop/urls.py
@@ -14,5 +14,4 @@ urlpatterns = [
     path('order_success', views.order_success, name='order_success'),
     # WEBHOOKS
     path('stripe_webhooks', views.stripe_webhooks, name='stripe_webhooks'),
-    path('email', views.email, name='email'),
 ]
